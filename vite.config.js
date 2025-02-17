665d3ac6e0f92e2fec977f0e507acc8187daf4b7
Noble <abrahamnobleofficial@gmail.com>
2025-02-18 00:53:59 +0530

    Initial commit

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,7 @@
-// vite.config.js
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  base: '/Rract-Routing/', // Make sure this matches your GitHub repository name
+  base: '/Rract-Routing/'
 });
